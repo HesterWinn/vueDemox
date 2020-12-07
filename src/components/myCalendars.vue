7ae6fbac10cf8b1875fb512978d19da9e088ba08
万惠娴 <179027145@qq.com>
2020-12-08 00:21:02 +0800

    添加头部

--- src/components/myCalendars.vue
+++ src/components/myCalendars.vue
@@ -1,39 +1,49 @@
 <template>
-  <section class="calendar-wrap">
-    <el-calendar v-model="timeValue" id="calendar">
-      <template slot="dateCell" slot-scope="{date, data}">
-        <!--自定义内容-->
-        <div @click="changeTime(date,data)">
-          <div class="calendar-day">{{ data.day.split('-').slice(2).join('-') }}</div>
-          <div v-for="(item, index) in calendarData" :key="index">
-            <div v-if="item === data.day">
-              <el-badge is-dot class="item"></el-badge>
+  <el-container>
+    <!-- 头部 -->
+    <main-header :HeaderTitle="headerTitle"></main-header>
+    <section class="calendar-wrap">
+      <el-calendar v-model="timeValue" id="calendar">
+        <template slot="dateCell" slot-scope="{date, data}">
+          <!-- 日程日期 -->
+          <div @click="changeTime(date,data)">
+            <div class="calendar-day">{{ data.day.split('-').slice(2).join('-') }}</div>
+            <div v-for="(item, index) in calendarData" :key="index">
+              <div v-if="item === data.day">
+                <el-badge is-dot class="item"></el-badge>
+              </div>
             </div>
           </div>
-        </div>
-      </template>
-    </el-calendar>
-    <div class="schedule-card">
-      <ul>
-        <li
-          v-for="(item, index) in scheduleList"
-          :key="index"
-        >
-          <h3>{{ item.exchange }}</h3>
-          <div class="title">{{ item.title }}</div>
-          <p class="content-txt">{{ item.content }}</p>
-        </li>
-      </ul>
-    </div>
-  </section>
+        </template>
+      </el-calendar>
+      <div class="schedule-card">
+        <ul>
+          <li
+            v-for="(item, index) in scheduleList"
+            :key="index"
+          >
+            <h3>{{ item.exchange }}</h3>
+            <div class="title">{{ item.title }}</div>
+            <p class="content-txt">{{ item.content }}</p>
+          </li>
+        </ul>
+      </div>
+    </section>
+  </el-container>
 </template>
 
 <script>
 import moment from 'moment'
+import mainHeader from '@/layout/header'
+
 export default {
   name: 'HelloWorld',
+  components: {
+    mainHeader
+  },
   data () {
     return {
+      headerTitle: '日历',
       timeValue: new Date(),
       calendarData: ['2020-12-11','2020-12-12'],
       scheduleList: []
---
+++ src/layout/header.vue
@@ -0,0 +1,62 @@
+<template>
+	<!-- 头部 -->
+	<header>
+        <el-button
+            onclick="javascript:history.back(-1);"
+            type="text"
+            icon="el-icon-arrow-left">
+        </el-button>
+		<h2>{{this.HeaderTitle}}</h2>
+	</header>
+</template>
+
+<script>
+	export default {
+		props: {
+            HeaderTitle: {
+                type: String,                      
+                default: function () {
+                    return ''
+                }
+            }
+        },
+		created: function() {
+			this.Load();
+		},
+		methods: {
+			Load: function(){
+				this.$nextTick(function () {
+				})
+			}
+		},
+	}
+</script>
+
+<style>
+body{
+    margin: 0;
+}
+header{
+	position: absolute;
+    top: 0!important;
+	width: 100%;
+	height: 3.5rem;
+	line-height: 3.5rem;
+	text-align: center;
+	background: rgba(208 64 64);
+	z-index: 100;
+    color: #fff;
+}
+header .el-button{
+	position: absolute;
+    left: 0;
+	width: 3.5rem;
+	height: 3.5rem;
+    color: #fff;
+}
+header h2{
+    margin: 0;
+	font-size: 1.1rem;
+	font-weight: 400;
+}
+</style>
